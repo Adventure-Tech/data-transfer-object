aa06071f29f931a87405c085fb9e5a5e28623b9b
Simen Mørch <simenmoerch@gmail.com>
2023-03-07 15:21:24 +0100

    fix: JsonMapper now triggers default method
    When the JsonMapper is finished with a POPO, it will try to call a
    method on the POPO called init.

--- src/JsonMapper/MapFromJsonToDto.php
+++ src/JsonMapper/MapFromJsonToDto.php
@@ -18,6 +18,7 @@ class MapFromJsonToDto
         $this->jsonDataString = $jsonDataString;
         $this->mapper = new JsonMapper();
         $this->mapper->bEnforceMapType = false;
+        $this->mapper->postMappingMethod = 'init';
     }
 
     /**
